4277f4c57526bf90092c65c6bdd707c4b156962f
Ahmet Sait <git@ahmetsait.com>
2022-06-19 22:20:26 +0300

    Lots of stuff
    - Add fontawesome
    - Add modernizr TypeScript definitions
    - Add toolbar and statusbar
    - Tweak header
    - Add help window
    - Implement import/export json
    - Implement dijkstra with visual animations
    - Implement "select all" and "delete selected" and their keyboard shortcuts
    - Implement ScanDeselect
    - Draw after changes only instead of every frame
    - Node labels are initialized empty
    - randomHslColor returns RGB Hex instead of HSL string
    - Node positions are truncated to int
    - Node radius is rounded to 2 decimals
    - Enable user page scaling
    - Remove twitter card metatags
    - Add generated js files

---
+++ js/algorithms.js
@@ -0,0 +1,76 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const animationDelay = 300;
+let highlightedNodeIndex = -1;
+let highlightedEdge;
+function animationStep() {
+    return __awaiter(this, void 0, void 0, function* () {
+        draw(window.performance.now());
+        return new Promise(function (resolve) {
+            setTimeout(resolve, animationDelay);
+        });
+    });
+}
+function gatherConnections() {
+    let connections = Array(nodes.length);
+    for (let i = 0; i < connections.length; i++) {
+        connections[i] = [].slice();
+    }
+    for (const edge of edges) {
+        addItemUnique(connections[edge.nodeIndex1], edge.nodeIndex2);
+        addItemUnique(connections[edge.nodeIndex2], edge.nodeIndex1);
+    }
+    return connections;
+}
+function dijkstra() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let startNodeIndex;
+        if (selectedNodeIndices.length === 1)
+            startNodeIndex = selectedNodeIndices[0];
+        else
+            return;
+        let visited = Array(nodes.length).fill(false);
+        let connections = gatherConnections();
+        let distances = Array(nodes.length).fill(Infinity);
+        distances[startNodeIndex] = 0;
+        for (let i = 0; i < nodes.length; i++) {
+            const node = nodes[i];
+            node.label = distances[i] === Infinity ? "∞" : distances[i].toString();
+        }
+        yield resolveDijkstra(startNodeIndex, connections, visited, distances, true);
+        highlightedNodeIndex = -1;
+        highlightedEdge = undefined;
+        draw(window.performance.now());
+    });
+}
+function resolveDijkstra(currentNodeIndex, connections, visited, distances, animate) {
+    return __awaiter(this, void 0, void 0, function* () {
+        highlightedNodeIndex = currentNodeIndex;
+        highlightedEdge = undefined;
+        if (animate)
+            yield animationStep();
+        for (const nodeIndex of connections[currentNodeIndex]) {
+            highlightedEdge = new GraphEdge(currentNodeIndex, nodeIndex);
+            if (animate)
+                yield animationStep();
+            if (distances[currentNodeIndex] + 1 < distances[nodeIndex]) {
+                distances[nodeIndex] = distances[currentNodeIndex] + 1;
+                nodes[nodeIndex].label = distances[nodeIndex] === Infinity ? "∞" : distances[nodeIndex].toString();
+                yield resolveDijkstra(nodeIndex, connections, visited, distances, animate);
+                highlightedNodeIndex = currentNodeIndex;
+                highlightedEdge = undefined;
+                if (animate)
+                    yield animationStep();
+            }
+        }
+    });
+}
+//# sourceMappingURL=algorithms.js.map
---
+++ js/vendor/modernizr.js
@@ -0,0 +1,10 @@
+"use strict";
+// Type definitions for Modernizr 3.5
+// Project: http://modernizr.com/, https://github.com/modernizr/modernizr
+// Definitions by: Boris Yankov <https://github.com/borisyankov>
+//                 Theodore Brown <https://github.com/theodorejb>
+//                 Leon Yu <https://github.com/leonyu>
+//                 Luca Trazzi <https://github.com/lucax88x>
+//                 Nathan Hardy <https://github.com/nhardy>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+//# sourceMappingURL=modernizr.js.map
